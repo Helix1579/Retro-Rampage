da480762438dde694446c2ddad45795a718ff4f0
Yashraj Mashruwala <105281067+Helix1579@users.noreply.github.com>
2025-05-17 14:34:06 +0200

    Revert "Healthbar done and Linked to Player"
    This reverts commit 6537bec5c50328fb9228de21d787a8aaab44fb0c.

--- Assets/Scripts/BaseHealth.cs
+++ Assets/Scripts/BaseHealth.cs
@@ -5,12 +5,9 @@ public abstract class BaseHealth : MonoBehaviour, IHealth
     public int maxHealth = 5;
     protected int currentHealth;
 
-    public System.Action<int> OnHealthChanged;
-
     protected virtual void Start()
     {
         currentHealth = maxHealth;
-        OnHealthChanged?.Invoke(currentHealth);
     }
 
     public virtual void TakeDamage(int amount)
@@ -18,8 +15,6 @@ public abstract class BaseHealth : MonoBehaviour, IHealth
         currentHealth -= amount;
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
         Debug.Log($"{gameObject.name} took {amount} damage. Remaining: {currentHealth}");
-
-        OnHealthChanged?.Invoke(currentHealth); // <- notify healthbar
 
         if (currentHealth <= 0)
         {
@@ -31,7 +26,6 @@ public abstract class BaseHealth : MonoBehaviour, IHealth
     {
         currentHealth += amount;
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
-        OnHealthChanged?.Invoke(currentHealth); // <- notify healthbar
     }
 
     protected abstract void Die();
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,21 +2,6 @@ using UnityEngine;
 
 public class PlayerHealth : BaseHealth
 {
-    public HealthBar healthBar;
-
-    protected override void Start()
-    {
-        base.Start();
-
-        if (healthBar != null)
-        {
-            healthBar.SetMaxHealth(maxHealth);
-            healthBar.SetHealth(currentHealth);
-
-            OnHealthChanged += healthBar.SetHealth;
-        }
-    }
-
     protected override void Die()
     {
         Debug.Log("Player died!");
