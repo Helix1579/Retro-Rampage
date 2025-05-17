33adc264725056336318f782d438b2ac89edf774
Yashraj Mashruwala <105281067+Helix1579@users.noreply.github.com>
2025-05-17 14:42:54 +0200

    Player Health Bar Linked

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,6 +2,21 @@ using UnityEngine;
 
 public class PlayerHealth : BaseHealth
 {
+    public HealthBar healthBar;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        if (healthBar != null)
+        {
+            healthBar.SetMaxHealth(maxHealth);
+            healthBar.SetHealth(currentHealth);
+
+            OnHealthChanged += healthBar.SetHealth;
+        }
+    }
+
     protected override void Die()
     {
         Debug.Log("Player died!");
